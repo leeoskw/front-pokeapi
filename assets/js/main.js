d255db4289752b1cb531f7eb9c0ca2a9f0d3c856
Leonardo <leeo_skw@hotmail.com>
2024-01-07 16:43:53 -0300

    :sparkles: feat: adiciona limite de requests e botão para carregar mais

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,11 @@
+const pokemonListHtml = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
+
+const limit = 5
+let offset = 0
+const maxRecords = 11
+
+
 function convertPokemonToHtmlLi(pokemon){
     // debugger
     return `
@@ -15,16 +23,40 @@ function convertPokemonToHtmlLi(pokemon){
     `
 }
 
-const pokemonListHtml = document.getElementById('pokemonList')
 
-pokeApi.getPokemons().then((pokemons = []) => {
-    // Código abaixo tem o mesmo efeito 
+// pokeApi.getPokemons().then((pokemons = []) => {
+//     // Código abaixo tem o mesmo efeito 
     
-    // const newPokemonListLiItems = pokemons.map( (pokemon, index, array) =>  convertPokemonToHtmlLi(pokemon))
-    // const pokemonLiItemsFiltered = newPokemonListLiItems.join('')
-    // pokemonListHtml.innerHTML += pokemonLiItemsFiltered
+//     // const newPokemonListLiItems = pokemons.map( (pokemon, index, array) =>  convertPokemonToHtmlLi(pokemon))
+//     // const pokemonLiItemsFiltered = newPokemonListLiItems.join('')
+//     // pokemonListHtml.innerHTML += pokemonLiItemsFiltered
 
     
-    // pokemonListHtml.innerHTML += pokemons.map((value) => convertPokemonToHtmlLi(value)).join('')
-    pokemonListHtml.innerHTML += pokemons.map(convertPokemonToHtmlLi).join('')
+//     // pokemonListHtml.innerHTML += pokemons.map((value) => convertPokemonToHtmlLi(value)).join('')
+//     pokemonListHtml.innerHTML += pokemons.map(convertPokemonToHtmlLi).join('')
+// })
+
+function loadPokemonItems(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map(convertPokemonToHtmlLi).join('')
+        pokemonListHtml.innerHTML += newHtml
+    })
+}
+
+loadPokemonItems(offset, limit)
+
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    const countRecordsNextPage = offset + limit
+    
+    if (countRecordsNextPage >= maxRecords){
+        const newLimit = maxRecords - offset // 11 - 9 => 2
+        loadPokemonItems(offset, newLimit)
+        // remove botão
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+        return 
+    }
+    loadPokemonItems(offset, limit)
 })
+
